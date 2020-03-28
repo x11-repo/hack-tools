264be3fb23a72b2a6e2f7c94f3fb0ca3130a1895
Admin <Admin@MacBook-Pro-Admin.local>
2020-03-28 14:06:48 +0300

    🔹 Выход из Alpha в 0.5 Beta! 🔹

---
+++ meenuuu/m11m11m11.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+from time import sleep
+from colorama import Fore, Back, Style
+os.system("clear")
+print(Fore.GREEN+f"    ╔╗ ╔╗        ╔╗  ╔════╗        ╔╗")
+print(Fore.GREEN+f"    ║║ ║║        ║║  ║╔╗╔╗║        ║║")
+print(Fore.GREEN+f"    ║╚═╝║╔══╗╔══╗║║╔╗╚╝║║╚╝╔══╗╔══╗║║ ╔══╗")
+print(Fore.GREEN+f"    ║╔═╗║║╔╗║║╔═╝║╚╝╝  ║║  ║╔╗║║╔╗║║║ ║══╣")
+print(Fore.GREEN+f"    ║║ ║║║╔╗║║╚═╗║╔╗╗  ║║  ║╚╝║║╚╝║║╚╗╠══║")
+print(Fore.GREEN+f"    ╚╝ ╚╝╚╝╚╝╚══╝╚╝╚╝  ╚╝  ╚══╝╚══╝╚═╝╚══╝{Style.RESET_ALL}")
+print(Fore.BLUE+f"                               a.k.a Kali Linux")
+print(Fore.BLUE+f"╔══════════════════════════════════════════════╗")
+print(Fore.BLUE+f"║{Style.RESET_ALL} Created By x11repo a.k.a Aqua                {Fore.BLUE}║"+Style.RESET_ALL)
+print(Fore.BLUE+f"║{Style.RESET_ALL} Telegram: https://teleg.run/hacktools666     {Fore.BLUE}║"+Style.RESET_ALL)
+print(Fore.BLUE+f"║{Style.RESET_ALL} Ver: 0.5 Beta                                {Fore.BLUE}║"+Style.RESET_ALL)
+print(Fore.BLUE+f"{Fore.BLUE}╠══════════════════════════════════════════════╣")
+print(Fore.BLUE+f"║{Style.RESET_ALL} {Fore.RED}[!]{Style.RESET_ALL} Перед использованием                     {Fore.BLUE}║\n║{Style.RESET_ALL}  установите все репозитории! {Fore.BLUE}                ║\n║ {Style.RESET_ALL} Для этого введите {Fore.GREEN}999                       {Fore.BLUE}║"+Style.RESET_ALL)
+print(Fore.BLUE+f"╠══════════════════════════════════════════════╣")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}1. Сбор информации {Fore.BLUE}                          ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}2. Анализ уязвимостей {Fore.BLUE}                       ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}3. Оценка базы данных{Fore.BLUE}                        ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}4. Атаки с помощью паролей{Fore.BLUE}                   ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}5. Беспроводные атаки{Fore.BLUE}                        ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}6. Инженерный анализ{Fore.BLUE}                         ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}7. Инструменты эксплуатации{Fore.BLUE}                  ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}8. Сниффинг и спуффинг{Fore.BLUE}                       ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}9. Пост эксплуатации{Fore.BLUE}                         ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}10. Реторика{Fore.BLUE}                                 ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}11. Инструменты отчетности{Fore.BLUE}                   ║")
+print(Fore.BLUE+f"          {Fore.YELLOW}0111) cherrytree"+Style.RESET_ALL) # sudo cherrytree
+print(Fore.YELLOW+f"          0211) xrdp"+Style.RESET_ALL) ##  sudo xrdp -h
+print(Fore.YELLOW+f"          0311) aircrack-ng"+Style.RESET_ALL) ##  aircrack-ng --help
+print(Fore.YELLOW+f"          0411) ardour5"+Style.RESET_ALL) ##  sudo ardour5
+print(Fore.BLUE+f"║ {Style.RESET_ALL}12. Стресс-тестирование{Fore.BLUE}                      ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}13. Системные сервисы{Fore.BLUE}                        ║")
+print(Fore.BLUE+f"╠══════════════════════════════════════════════╣")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}Для обновления введите '{Fore.GREEN}7777{Style.RESET_ALL}'{Fore.BLUE}                ║")
+print(Fore.BLUE+f"║ {Style.RESET_ALL}Если вы хотите сообщить о баге или           {Fore.BLUE}║\n║{Style.RESET_ALL} хотите что-то предложить - введите '{Fore.GREEN}007{Style.RESET_ALL}'   {Fore.BLUE}  ║")
+print(Fore.BLUE+f"╚══════════════════════════════════════════════╝")
---
+++ meenuuu/setup0.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import os
+import time
+from time import sleep
+from colorama import Fore, Back, Style
+print(Fore.BLUE+f"\n========================================================"+Style.RESET_ALL)
+print("[INFO] Можешь свернуть терминал и идти смотреть мультики...")
+print("Потому-что это займёт много времени.")
+print(Fore.BLUE+f"========================================================"+Style.RESET_ALL)
+print(Fore.WHITE+f"{Back.RED}\nНо лучше следить за установкой, так-как при клонировании будет спрашивать подтверждение."+Style.RESET_ALL)
+time.sleep(8)
+os.system("apt update")
+os.system("apt install -y git")
+os.system("apt install -y python")
+os.system("apt install -y python2")
+os.system("apt install -y wget")
+os.system("apt install -y nmap")
+os.system("plg install -y hydra ")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install python2")
+os.system("cd && git clone https://github.com/sqlmapproject/sqlmap.git")
+os.system("cd")
+os.system("apt install wget")
+os.system("cd && wget https://Auxilus.github.io/metasploit.sh")
+os.system("cd && bash metasploit.sh")
+os.system("cd && cd metasploit-framework")
+os.system("cd && gem install bundle --pre")
+os.system("cd && bundle config build.nokogiri --use-system-libraries")
+os.system("cd && bundle install")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install -y python2")
+os.system("cd && git clone https://github.com/ihebski/angryFuzzer.git")
+os.system("cd && cd angryFuzzer")
+os.system("cd && pip2 install -r requirements.txt")
+os.system("cd && pip2 install requests")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install -y php")
+os.system("cd && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y python2")
+os.system("apt install -y git")
+os.system("cd && git clone https://github.com/evait-security/weeman.git")
+os.system("cd && cd weeman")
+os.system("cd && chmod +x weeman.py")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install -y python")
+os.system("cd && git clone https://github.com/maldevel/IPGeoLocation.git")
+os.system("cd && cd IPGeoLocation")
+os.system("cd && pip install -r requirements.txt")
+os.system("cd")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install -y python")
+os.system("cd && git clone https://github.com/Mebus/cupp.git")
+os.system("apt update -y")
+os.system("apt install -y git")
+os.system("apt install -y python")
+os.system("apt install -y nano")
+os.system("pip install requests")
+os.system("cd && git clone https://github.com/reverse-shell/routersploit.git")
+os.system("cd && cd routersploit")
+os.system("pip2 install -r requirements.txt")
+os.system("pip2 install -r requirements-dev.txt")
+os.system("pip2 install -r requests")
+os.system("cd && apt install p0f") ### 1 пункт
+os.system("cd && apt install arp-scan") ### sudo arp-scan
+os.system("cd && apt install cutycapt") ## sudo cutycapt
+os.system("cd && apt install python-elixir python-qt4 xsltproc")
+os.system("cd && git clone https://github.com/secforce/sparta.git") ## python sparta.py
+os.system("cd && git clone https://github.com/golismero/golismero.git") ## python golismero.py
+os.system("cd && apt install masscan") ## masscan
+os.system("cd && wget https://github.com/`curl -s https://github.com/ron190/jsql-injection/releases | grep -E -o '/ron190/jsql-injection/releases/download/v[0-9]{1,2}.[0-9]{1,2}/jsql-injection-v[0-9]{1,2}.[0-9]{1,2}.jar' | head -n 1`") ## java -jar ./jsql-injection-v*.jar
+os.system("cd && apt install openvas") #### 2 punkt ## openvas-start
+os.system("cd && git clone https://github.com/SpiderLabs/jboss-autopwn.git") ## ./e2.sh ## 3 punkt
+os.system("cd && apt install dirb") ## dirb
+os.system("cd && git clone https://github.com/iniqua/plecost.git && cd plecost && chmod +x * && python3 setup.py install") ##plecost
+os.system("cd && gem install wpscan && wpscan --update --disable-tls-checks") ## wpscan
+os.system("cd && git clone https://github.com/epsylon/xsser.git && cd && cd xsser && sudo python3 setup.py install && sudo apt install python3-pycurl python3-bs4 python3-geoip python3-geoip2 python3-gi python3-cairocffi python3-selenium firefoxdriver && sudo pip3 install pycurl bs4 geoip2 gobject cairocffi selenium") ## xsser
+os.system("cd && sudo apt install crunch") ## 4 punkt ## crunch
+## hashcat -- cd hack-tools/assets/hashcat/ && ./hashcat64.bin --help
+os.system("cd && wget http://www.openwall.com/john/g/john-1.7.9.tar.bz2 && wget http://www.openwall.com/john/g/john-1.7.9.tar.bz2.sign && wget http://www.openwall.com/signatures/openwall-signatures.asc && pgp -ka openwall-signatures.asc && pgp john-1.7.9.tar.bz2.sign john-1.7.9.tar.bz2 && sudo apt install john") ## john
+os.system("cd && sudo apt install ncrack") ## ncrack
+os.system("cd && sudo apt install aircrack-ng")## 5 punkt ## aircrack-ng
+os.system("cd && sudo apt install git build-essential libssl-dev libpcap-dev && git clone https://github.com/joswr1ght/asleap && cd && cd asleap-2.2 && make")## cd asleap-2.2 && ./asleap
+os.system("cd && sudo apt install libpcap-dev && wget http://www.willhackforsushi.com/code/cowpatty/4.6/cowpatty-4.6.tgz && tar xvzf cowpatty-*.tgz && cd cowpatty-4.6 && make && sudo make install")### cd cowpatty-4.6 && ./cowpatty
+os.system("cd && sudo apt install mdk3") ## sudo mdk3
+os.system("cd && sudo apt install reaver") ## sudo reaver
+os.system("cd && sudo apt install wifite") ## sudo wifite
+os.system("cd && sudo apt install yara") ## 6 punkt ## sudo yara
+os.system("cd && curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall") ## 7 punkt
+###!!!! НАДО ДОБАВИТЬ МЕТАСПЛОИТ И АРМИТАЖ !!!!###
+os.system("cd && git clone https://github.com/trustedsec/social-engineer-toolkit/ set/ && cd && cd set && chmod +x * && sudo python setup.py") ## setoolkit
+os.system("cd && apt install python3-pip && git clone https://www.github.com/threat9/routersploit && cd routersploit && python3 -m pip install -r requirements.txt") ## cd routersploit && python3 rsf.py
+os.system("cd && sudo apt install yersinia") ##sudo yersinia -h
+os.system("cd && sudo apt install ettercap-graphical")## 8 punkt ## sudo ettercap -h
+os.system("cd && sudo apt install sslstrip") ## sudo sslstrip -h
+os.system("cd && sudo apt install dsniff") ## sudo dsniff -h
+os.system("cd && sudo apt install wireshark") ## sudo wireshark
+os.system("cd && sudo apt install dns2tcp") ## 9 punkt ## sudo dns2tcpd
+os.system("cd && sudo apt install sbd") ## sudo sbd
+os.system("cd && sudo apt install nghttp2") ## sudo nghttpd
+os.system("cd && sudo apt install kdiff3")## 10 punkt ## sudo kdiff3 
+############## p0f
+os.system("cd && sudo apt install xpdf") ## sudo xpdf
+os.system("cd && sudo apt install libimage-exiftool-perl") ## sudo exiftool
+os.system("cd && sudo apt install cherrytree")## 11 punkt ## sudo cherrytree
+os.system("cd && sudo apt install xrdp && sudo systemctl enable xrdp") ## sudo xrdp -h
+############## aircrack-ng --help
+os.system("cd && sudo apt install ardour") ## sudo ardour5
+os.system("cd && sudo apt install dhcpig") ##12 punkt ## sudo dhcpig
+os.system("cd && sudo apt install t50") ## sudo t50 -h
+os.system("cd && sudo apt install terminator") ## sudo terminator
+os.system("cd && sudo apt install thc-ipv6") ## больше не поддерживается
+os.system("cd && sudo apt install python3-crcelk python3-pluginbase python3-pyasn1 python3-serial python3-smoke-zephyr python3-tabulate python3-termcolor && sudo pip3 install termineter")
+############## sudo termineter
